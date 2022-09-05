c82e9c615c677026a4d691dbfde483c66c2042e9
rankothnc <ncrankoth@gmail.com>
2022-09-05 14:59:41 +0530

    created a table

---
+++ server/index.js
@@ -0,0 +1,31 @@
+//create express server
+const express = require("express");
+//create app through express server
+const app = express();
+
+const mysql = require('mysql');
+
+//create DB
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "cruddatabase"
+})
+
+app.get("/", (req, res) => {
+
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReviews) VALUES('inception', 'good movie')"
+
+    //to enter sql commantds
+    //result-send information to frontend  
+    db.query(sqlInsert, (err, result) => {
+        //when enter data to the table, it'll display this
+        res.send("Hi Chinthani Rankoth");
+    })
+});
+
+app.listen(3001, () => {
+    console.log("Running on port 3001")
+});
+
